53a07d848005e8ad7c008c8591ba2d2043f6fa72
tand100b <95879253+tand100b@users.noreply.github.com>
2022-02-23 16:55:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    const x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  const changeColor = (event) => {
+    document.body.dataset.background = event.target.dataset.color;
+  };
+  
+  document.querySelectorAll('.color-picker')
+    .forEach(e => e.addEventListener('click', changeColor));
+    
+  
